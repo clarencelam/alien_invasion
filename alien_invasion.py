bfa443919aa52ddee6a55720d402351a359d89e5
clam <lam.clarence.bc@gmail.com>
2021-11-04 14:08:11 -0700

    started project

---
+++ alien_invasion.py
@@ -0,0 +1,151 @@
+import sys, pygame
+
+from settings import Settings
+from ship import Ship
+from bullet import Bullet
+from alien import Alien
+
+class AlienInvasion:
+	"""overall class to manage game assets & behavior."""
+
+	def __init__(self):
+		"""initialize a game, create game resoures"""
+		pygame.init()
+
+		self.settings = Settings()
+
+		self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+		self.settings.screen_width = self.screen.get_rect().width
+		self.settings.screen_height = self.screen.get_rect().height #this code exists so we can use the screen's rect width/height to know what the screen width/height ends up being
+			#(self.settings.screen_width, self.settings.screen_height))
+		pygame.display.set_caption("Alien Invasion")
+
+		self.ship = Ship(self)
+		self.bullets = pygame.sprite.Group()
+		self.aliens = pygame.sprite.Group()
+
+		self._create_fleet()
+
+	def run_game(self):
+		""" start the main loop for the game"""
+		while True:
+			self._check_events()
+			self.ship.update()
+			self._update_bullet()
+			self._update_aliens()
+			self._update_screen()
+
+	def _check_events(self):
+		"""respond to keypresses and mouse events"""
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				sys.ext()
+			elif event.type == pygame.KEYDOWN:
+				self._check_keydown_events(event)
+			elif event.type == pygame.KEYUP:
+				self._check_keyup_events(event)
+
+	def _check_keydown_events(self, event):
+		"""respond to key press-down events"""
+		if event.key == pygame.K_RIGHT:
+			self.ship.moving_right = True
+		elif event.key == pygame.K_LEFT:
+			self.ship.moving_left = True
+		elif event.key == pygame.K_SPACE:
+			self._fire_bullet()
+		elif event.key == pygame.K_q:
+			sys.exit()
+
+	def _check_keyup_events(self, event):
+		"""responds to key-up / key release events"""
+		if event.key == pygame.K_RIGHT:
+			self.ship.moving_right = False
+		elif event.key == pygame.K_LEFT:
+			self.ship.moving_left = False
+
+	def _create_fleet(self):
+		""" Create the fleet of aliens """
+		# Make an Alien and find the number of aliens in a row
+		# Spacing between each alien is equal to one alien width
+		alien = Alien(self)
+		alien_height = alien.rect.height
+		alien_width = alien.rect.width
+		available_space_x = self.settings.screen_width - (2 * alien_width)
+		number_aliens_x = available_space_x // (2 * alien_width)
+
+		# Determine the number of rows of aliens that fit on the screen
+		ship_height = self.ship.rect.height 
+		available_space_y = (self.settings.screen_height - (3 * alien_height) - ship_height)
+
+		number_rows = available_space_y // (2 * alien_height)
+
+		# Create the fleet of aliens.
+		for row_number in range(number_rows):
+			for alien_number in range(number_aliens_x):
+				# Create an alien and place it in the row
+				self._create_alien(alien_number, row_number)
+
+	def _create_alien(self, alien_number, row_number):
+		# Create an alien and place it in the row
+		alien = Alien(self)
+		alein_height = alien.rect.height
+		alien_width = alien.rect.width
+		alien.x = alien_width + 2 * alien_width * alien_number
+		alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number 
+		alien.rect.x = alien.x 
+		self.aliens.add(alien) 
+
+	def _check_fleet_edges(self):
+		""" Respond appropriately if any aliens touched the edge"""
+		for alien in self.aliens.sprites():
+			if alien.check_edges():
+				self._change_fleet_direction()
+				break
+
+	def _change_fleet_direction(self):
+		""" Drop the alien fleet and change directions""" 
+		for alien in self.aliens.sprites():
+			alien.rect.y += self.settings.fleet_drop_speed
+		self.settings.fleet_direction = self.settings.fleet_direction * -1 #when the alien hits the edge, the fleet direction is shifted
+
+	def _fire_bullet(self):
+		""" function to fire bullet from ship """
+		if len(self.bullets) < self.settings.bullets_allowed:
+			new_bullet = Bullet(self) #making a bullet instance
+			self.bullets.add(new_bullet)
+
+
+	def _update_bullet(self):
+		""" Update the position of bullets and get rid of old bullets"""
+		# Update bullet positions
+		self.bullets.update()
+
+		# Get rid of bullets that have hit the top of the screen
+		for bullet in self.bullets.copy(): # we can't use the real bullets list because python expects a list to stay the same when a loop is running
+			if bullet.rect.bottom <= 0:
+				self.bullets.remove(bullet)
+		# print(len(self.bullets)) ----- deleted this line as it is only useful to see if the above loop works to delete bullets (it works)
+
+	def _update_aliens(self):
+		""" check if the Fleet is at an edge. Then update positions of all aliens in the fleet"""
+		self._check_fleet_edges()
+		self.aliens.update()
+
+	def _update_screen(self):
+		"""update images on the screen, and flip to the new screen"""
+		self.screen.fill(self.settings.bg_color)
+		self.ship.blitme()
+		for bullet in self.bullets.sprites():
+			bullet.draw_bullet()
+		self.aliens.draw(self.screen)
+
+		# make the most recently drawn screen visible
+		pygame.display.flip() 
+
+
+			
+if __name__ == '__main__':
+	# Make a game instance, and run the game
+	ai = AlienInvasion()
+	ai.run_game()
+
---
+++ settings.py
@@ -0,0 +1,26 @@
+class Settings:
+	""" A class to store all settings for Alien Invasion """
+
+	def __init__(self):
+		"""initialize the game settings"""
+		# Screen Settings
+		self.screen_width = 1600
+		self.screen_height = 1000
+		self.bg_color = (100,100,100)
+
+		# Ship settings
+		self.ship_speed = 1.5
+		
+		# Bullet Settings 
+		self.bullet_speed = 1.0
+		self.bullet_width = 3
+		self.bullet_height = 15
+		self.bullet_color = (60, 60, 60)
+		self.bullets_allowed = 3
+
+		# Alien Settings
+		self.alien_speed = 1.0
+		self.fleet_drop_speed = 10
+		# fleet direction of 1 = going right (forward)
+		self.fleet_direction = 1                                                                                                                                                                                                                                                                   
+		
