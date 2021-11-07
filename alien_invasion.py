613c34153412ea813930c15f5e5871479d2cf3b5
clam <lam.clarence.bc@gmail.com>
2021-11-06 18:08:44 -0700

    implement game over conditions

--- alien_invasion.py
+++ alien_invasion.py
@@ -36,9 +36,12 @@ class AlienInvasion:
 		""" start the main loop for the game"""
 		while True:
 			self._check_events()
-			self.ship.update()
-			self._update_bullets()
-			self._update_aliens()
+
+			if self.stats.game_active:
+				self.ship.update()
+				self._update_bullets()
+				self._update_aliens()
+			
 			self._update_screen()
 
 	def _check_events(self):
@@ -132,19 +135,22 @@ class AlienInvasion:
 
 	def _ship_hit(self):
 		""" Respond to the ship being hit by an alien"""
-		# Decreemnt ships_left
-		self.stats.ships_left -= 1
+		if self.stats.ships_left > 0:
+			# Decreemnt ships_left
+			self.stats.ships_left -= 1
 
-		# Get rid of any remaining bullets & aliens
-		self.aliens.empty()
-		self.bullets.empty()
+			# Get rid of any remaining bullets & aliens
+			self.aliens.empty()
+			self.bullets.empty()
 
-		# Create a new fleet and center the ship
-		self._create_fleet()
-		self.ship.center_ship()
+			# Create a new fleet and center the ship
+			self._create_fleet()
+			self.ship.center_ship()
 
-		# Pause
-		sleep(0.5)
+			# Pause
+			sleep(0.5)
+		else:
+			self.stats.game_active = False
 
 
 	def _update_bullets(self):
@@ -159,6 +165,15 @@ class AlienInvasion:
 
 		self._check_bullet_alien_collisions()
 
+	def _check_aliens_bottom(self):
+		""" check if any aliens hae reached the bottom of the ship."""
+		screen_rect = self.screen.get_rect()
+		for alien in self.aliens.sprites():
+			if alien.rect.bottom >= screen_rect.bottom:
+				# Treat this like the ship got hit
+				self._ship_hit()
+				break
+
 	def _update_aliens(self):
 		""" check if the Fleet is at an edge. Then update positions of all aliens in the fleet"""
 		self._check_fleet_edges()
@@ -167,6 +182,7 @@ class AlienInvasion:
 		# Look for alien-ship collisions
 		if pygame.sprite.spritecollideany(self.ship, self.aliens):
 			self._ship_hit()
+		self._check_aliens_bottom()
 
 	def _update_screen(self):
 		"""update images on the screen, and flip to the new screen"""
--- game_stats.py
+++ game_stats.py
@@ -6,8 +6,11 @@ class GameStats:
 		self.settings = ai_game.settings
 		self.reset_stats()
 
+		# Start Alien Invasion in an active state
+		self.game_active = True
+
+
 	def reset_stats(self):
 		""" initialize statistics that can change during the game"""
 		self.ships_left = self.settings.ship_limit
 
-	
