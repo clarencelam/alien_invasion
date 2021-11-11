208fbd956ad8acb124f09dd38f06e010e7d159a7
clam <lam.clarence.bc@gmail.com>
2021-11-10 20:58:31 -0800

    implemented replay button

--- alien_invasion.py
+++ alien_invasion.py
@@ -64,7 +64,18 @@ class AlienInvasion:
 	def _check_play_button(self, mouse_pos):
 		""" Start a new game when the player clicks Play"""
 		if self.play_button.rect.collidepoint(mouse_pos):
+			# Reset the game stats
+			self.stats.reset_stats()
 			self.stats.game_active = True
+
+			# Get rid of any remaining aliens / bullets
+			self.aliens.empty()
+			self.bullets.empty()
+
+			# Create a new fleet and center the ship
+			self._create_fleet
+			self.ship.center_ship()
+
 
 	def _check_keydown_events(self, event):
 		"""respond to key press-down events"""
@@ -163,6 +174,7 @@ class AlienInvasion:
 			sleep(0.5)
 		else:
 			self.stats.game_active = False
+
 
 
 	def _update_bullets(self):
--- settings.py
+++ settings.py
@@ -20,8 +20,8 @@ class Settings:
 		self.bullets_allowed = 50
 
 		# Alien Settings
-		self.alien_speed = 1.5
-		self.fleet_drop_speed = 10
+		self.alien_speed = 3
+		self.fleet_drop_speed = 20
 		# fleet direction of 1 = going right (forward)
 		self.fleet_direction = 1                                                                                                                                                                                                                                                                   
 		
